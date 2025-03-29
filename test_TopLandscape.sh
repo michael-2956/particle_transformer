50c7499cfde59a59a9fb349bbf53f12a588f234e
michael-2956 <work.michael.2956@gmail.com>
2025-03-29 12:28:51 +0200

    test file change

--- test_TopLandscape.sh
+++ test_TopLandscape.sh
@@ -52,7 +52,7 @@ fi
 
 weaver \
     --predict \
-    --data-test "${DATADIR}/train_file.parquet" \
+    --data-test "${DATADIR}/val_file.parquet" \
     --data-config data/TopLandscape/top_${FEATURE_TYPE}.yaml --network-config $modelopts \
     --model-prefix trained_models/patT_93975.pt \
     --num-workers 1 --fetch-step 1 --in-memory \
