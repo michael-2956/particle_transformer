952b6347c552e5bdf2a2c254c32d156df5a73d8f
michael-2956 <work.michael.2956@gmail.com>
2025-03-31 22:42:34 +0300

    focal loss code

---
+++ networks/example_ParticleTransformer_AlteredLoss.py
@@ -0,0 +1,82 @@
+import torch
+import torch.nn.functional as F
+from weaver.nn.model.ParticleTransformer import ParticleTransformer
+from weaver.utils.logger import _logger
+
+'''
+Link to the full model implementation:
+https://github.com/hqucms/weaver-core/blob/main/weaver/nn/model/ParticleTransformer.py
+'''
+
+
+class ParticleTransformerWrapper(torch.nn.Module):
+    def __init__(self, **kwargs) -> None:
+        super().__init__()
+        self.mod = ParticleTransformer(**kwargs)
+
+    @torch.jit.ignore
+    def no_weight_decay(self):
+        return {'mod.cls_token', }
+
+    def forward(self, points, features, lorentz_vectors, mask):
+        return self.mod(features, v=lorentz_vectors, mask=mask)
+
+
+def get_model(data_config, **kwargs):
+
+    cfg = dict(
+        input_dim=len(data_config.input_dicts['pf_features']),
+        num_classes=len(data_config.label_value),
+        # network configurations
+        pair_input_dim=4,
+        use_pre_activation_pair=False,
+        embed_dims=[128, 512, 128],
+        pair_embed_dims=[64, 64, 64],
+        num_heads=8,
+        num_layers=8,
+        num_cls_layers=2,
+        block_params=None,
+        cls_block_params={'dropout': 0, 'attn_dropout': 0, 'activation_dropout': 0},
+        fc_params=[],
+        activation='gelu',
+        # misc
+        trim=True,
+        for_inference=False,
+    )
+    cfg.update(**kwargs)
+    _logger.info('Model config: %s' % str(cfg))
+
+    model = ParticleTransformerWrapper(**cfg)
+
+    model_info = {
+        'input_names': list(data_config.input_names),
+        'input_shapes': {k: ((1,) + s[1:]) for k, s in data_config.input_shapes.items()},
+        'output_names': ['softmax'],
+        'dynamic_axes': {**{k: {0: 'N', 2: 'n_' + k.split('_')[0]} for k in data_config.input_names}, **{'softmax': {0: 'N'}}},
+    }
+
+    return model, model_info
+
+
+def focal_loss(inputs, targets, alpha=1.0, gamma=2.0, reduction='mean'):
+    targets = targets.view(-1, 1)
+
+    log_probs = F.log_softmax(inputs, dim=1)
+    log_p_t = log_probs.gather(1, targets).view(-1)
+    
+    probs = torch.exp(log_probs)
+    # gathers probabilities of true class according to target
+    p_t = probs.gather(1, targets).view(-1)
+
+    loss = -alpha * (1 - p_t) ** gamma * log_p_t
+
+    if reduction == 'mean':
+        loss = loss.mean()
+    elif reduction == 'sum':
+        loss = loss.sum()
+    
+    return loss
+
+
+def get_loss(data_config, **kwargs):
+    return focal_loss # torch.nn.CrossEntropyLoss()
