9505d40242539bfd3998f1adb38c82614b7c1825
michael-2956 <work.michael.2956@gmail.com>
2025-04-01 21:12:54 +0300

    ce loss that ignores outliers

--- networks/example_ParticleTransformer_AlteredLoss.py
+++ networks/example_ParticleTransformer_AlteredLoss.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn.functional as F
 from weaver.nn.model.ParticleTransformer import ParticleTransformer
 from weaver.utils.logger import _logger
-from .focal_loss import get_loss_focal, get_loss_inverse_focal
+from .alternative_losses import get_loss_focal, get_loss_inverse_focal, get_loss_nooutlier_cross_entropy
 from .example_ParticleTransformer import ParticleTransformerWrapper
 from .example_ParticleTransformer import get_model as get_model_ParT
 
@@ -12,4 +12,5 @@ def get_model(data_config, **kwargs):
 def get_loss(data_config, **kwargs): 
     # return torch.nn.CrossEntropyLoss()
     # return get_loss_focal(data_config, **kwargs)
-    return get_loss_inverse_focal(data_config, **kwargs)
+    # return get_loss_inverse_focal(data_config, **kwargs)
+    return get_loss_nooutlier_cross_entropy(data_config, **kwargs)
