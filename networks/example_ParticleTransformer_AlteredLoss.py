8046856b20c7deb544f8d3bda44ecc6eb0253d5b
michael-2956 <work.michael.2956@gmail.com>
2025-04-01 19:58:35 +0300

    inverse focal loss

--- networks/example_ParticleTransformer_AlteredLoss.py
+++ networks/example_ParticleTransformer_AlteredLoss.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn.functional as F
 from weaver.nn.model.ParticleTransformer import ParticleTransformer
 from weaver.utils.logger import _logger
-from .focal_loss import get_loss_focal
+from .focal_loss import get_loss_focal, get_loss_inverse_focal
 from .example_ParticleTransformer import ParticleTransformerWrapper
 from .example_ParticleTransformer import get_model as get_model_ParT
 
@@ -10,4 +10,6 @@ def get_model(data_config, **kwargs):
     return get_model_ParT(data_config, **kwargs)
 
 def get_loss(data_config, **kwargs): 
-    return get_loss_focal(data_config, **kwargs)  # torch.nn.CrossEntropyLoss()
+    # return torch.nn.CrossEntropyLoss()
+    # return get_loss_focal(data_config, **kwargs)
+    return get_loss_inverse_focal(data_config, **kwargs)
