8fcba9f2fa3578f26c0f55b8e5402bbfa30b3b7e
michael-2956 <work.michael.2956@gmail.com>
2025-04-05 21:03:22 +0300

    add part with inverter

--- test_TopLandscape.sh
+++ test_TopLandscape.sh
@@ -18,6 +18,9 @@ extraopts=""
 if [[ "$model" == "ParT" ]]; then
     modelopts="networks/example_ParticleTransformer.py --use-amp --optimizer-option weight_decay 0.01"
     lr="1e-3"
+if [[ "$model" == "ParT-Inverter" ]]; then
+    modelopts="networks/ParticleTransformerWithInverter.py --use-amp --optimizer-option weight_decay 0.01"
+    lr="1e-3"
 elif [[ "$model" == "ParT-Long" ]]; then
     modelopts="networks/example_ParticleTransformerLong.py --use-amp --optimizer-option weight_decay 0.01"
     lr="1e-3"
