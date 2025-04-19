fc59725aee91806af3369e55781ebcbb2135da13
michael-2956 <work.michael.2956@gmail.com>
2025-04-19 23:35:16 +0300

    tiny ParT

--- test_TopLandscape.sh
+++ test_TopLandscape.sh
@@ -33,6 +33,9 @@ elif [[ "$model" == "ParT-Big" ]]; then
 elif [[ "$model" == "ParT-Small" ]]; then
     modelopts="networks/example_ParticleTransformerSmall.py --use-amp --optimizer-option weight_decay 0.01"
     lr="1e-3"
+elif [[ "$model" == "ParT-Tiny" ]]; then
+    modelopts="networks/example_ParticleTransformerTiny.py --use-amp --optimizer-option weight_decay 0.01"
+    lr="1e-3"
 elif [[ "$model" == "ParT-AlteredLoss" ]]; then
     modelopts="networks/example_ParticleTransformer_AlteredLoss.py --use-amp --optimizer-option weight_decay 0.01"
     lr="1e-3"
