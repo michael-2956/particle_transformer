71839a2d0081002cf3422dbeee9d36a2506495bc
michael-2956 <work.michael.2956@gmail.com>
2025-04-22 01:46:59 +0300

    add even smaller and no dropout models

--- test_TopLandscape.sh
+++ test_TopLandscape.sh
@@ -36,6 +36,16 @@ elif [[ "$model" == "ParT-Tiny" ]]; then
     lr="3e-3"
     optimizer="soap"
     batch_size=2048
+elif [[ "$model" == "ParT-Tiny-NoDrop" ]]; then
+    modelopts="networks/ParticleTransformerTinyNoDropout.py --use-amp --optimizer-option weight_decay 0.01"
+    lr="3e-3"
+    optimizer="soap"
+    batch_size=2048
+elif [[ "$model" == "ParT-Pico-NoDrop" ]]; then
+    modelopts="networks/ParticleTransformerPicoNoDropout.py --use-amp --optimizer-option weight_decay 0.01"
+    lr="3e-3"
+    optimizer="soap"
+    batch_size=2048
 elif [[ "$model" == "ParT-AlteredLoss" ]]; then
     modelopts="networks/example_ParticleTransformer_AlteredLoss.py --use-amp --optimizer-option weight_decay 0.01"
 elif [[ "$model" == "ParT-Long-AlteredLoss" ]]; then
