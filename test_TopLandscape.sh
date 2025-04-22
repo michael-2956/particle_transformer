ed9de6f2b9801e7d479164483fdff1dfeac74efa
michael-2956 <work.michael.2956@gmail.com>
2025-04-23 00:20:18 +0300

    misc

--- test_TopLandscape.sh
+++ test_TopLandscape.sh
@@ -109,7 +109,7 @@ for part in train val test; do
         --in-memory \
         --batch-size $batch_size \
         --predict-gpus 0 \
-        --gpus 0,1 \
+        --gpus 0 \
         --log logs/TopLandscape_${model}_{auto}${suffix}.log \
         --predict-output pred_${part}_kaggle.root \
         --tensorboard TopLandscape_${FEATURE_TYPE}_${model}${suffix} \
