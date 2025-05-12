70fb09997d777b9493df873adced944b97227f83
michael-2956 <work.michael.2956@gmail.com>
2025-05-12 15:38:43 +0300

    4 gpus

--- scaling_experiments/neurons_per_head/setting_collections/setting_4.sh
+++ scaling_experiments/neurons_per_head/setting_collections/setting_4.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-./scaling_experiments/train_and_test_with_settings.sh ParT-nnph1 kin --num-neurons-per-head 1
+./scaling_experiments/train_and_test_with_settings.sh ParT-nnph1 kin --num-neurons-per-head 1 --gpus 0,1,2,3 --predict-gpus 0,1,2,3
 
