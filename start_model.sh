8d1a6e397171709fc6f62bbbfa46672b0dfad9fa
Oleh Prostakov <prostakov.oleh@gmail.com>
2025-06-05 16:13:49 +0300

    blah

--- start_model.sh
+++ start_model.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 ./scaling_experiments/train_and_test_with_settings.sh \
-ParT-esm0.5-samewts kin --embedding-scale-mult 0.5 \
---network-option identical_attn_weights True \
---network-option use_moe True
+    ParT-samewts-WLD-W-nl30-nlcm0.06-esm0.5-nnph16 kin \
+    --total-num-layers 30 \
+    --num-cls-layers-mult 0.06 \
+    --embedding-scale-mult 0.5 \
+    --network-option weighted_decode_every_layer True \
+    --network-option identical_attn_weights True \
+    --network-option use_moe False
