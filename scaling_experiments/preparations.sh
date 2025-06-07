827db777bd08f659bbc482fa9a08b95755d6518f
michael-2956 <work.michael.2956@gmail.com>
2025-06-07 20:39:54 +0300

    new preparations

--- scaling_experiments/preparations.sh
+++ scaling_experiments/preparations.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+BRANCH=${1:-main}
 
 # fail on error, unset variable,
 # or if any stage in a pipeline fails
@@ -16,10 +17,10 @@ dataset_path=$(realpath original_dataset)
 
 echo Cloning repositories \& installing dependencies ...
 
-echo Cloning weaver-core...
-git clone https://github.com/michael-2956/weaver-core.git &> /dev/null
-echo Cloning particle_transformer...
-git clone https://github.com/michael-2956/particle_transformer.git &> /dev/null
+echo Cloning $BRANCH of weaver-core...
+git clone --branch "$BRANCH" --depth 1 https://github.com/michael-2956/weaver-core.git &> /dev/null
+echo Cloning $BRANCH of particle_transformer...
+git clone --branch "$BRANCH" https://github.com/michael-2956/particle_transformer.git &> /dev/null
 echo Installing weaver-core...
 pip install -e weaver-core &> /dev/null
 
@@ -30,6 +31,13 @@ echo >> particle_transformer/env.sh
 echo "export DATADIR_JetClass=" >> particle_transformer/env.sh
 echo "export DATADIR_TopLandscape=${dataset_path}" >> particle_transformer/env.sh
 echo "export DATADIR_QuarkGluon=" >> particle_transformer/env.sh
+
+if [[ -e ./particle_transformer/sync_weaver.sh ]]; then
+  chmod +x ./particle_transformer/sync_weaver.sh
+fi
+if [[ -e ./particle_transformer/start_model.sh ]]; then
+  chmod +x ./particle_transformer/start_model.sh
+fi
 
 echo
 echo Ready to run!
